9f8389f499dcb7988e39d3ed06160b13dfa89a9d
Martin Woodward <martin@woodwardweb.com>
2019-11-04 09:52:07 -0500

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -15,7 +15,7 @@ jsr $e544
   jmp .loop
 
 ; the text to write                                     .
-message   !scr "               hallo wereld!            "
+message   !scr "               hallo ignite!            "
 
 .write_line
   ldx #$00
