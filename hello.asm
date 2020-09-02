098a2f050f289efb5130cbeebee876eab7fbbe87
Martin Woodward <martinwoodward@github.com>
2020-09-02 17:50:13 +0000

    Update message

--- hello.asm
+++ hello.asm
@@ -15,7 +15,7 @@ jsr $e544
   jmp .loop
 
 ; the text to write                                     .
-message   !scr "              hello k8s folks           "
+message   !scr "              hello mvps                "
 
 .write_line
   ldx #$00
